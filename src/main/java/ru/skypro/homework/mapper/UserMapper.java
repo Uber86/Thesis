a8379b1b60924e53a9ce70d3d4568fe44511ef5d
galina <galina.ramodina@gmail.com>
2025-04-26 09:21:15 +0300

    + UserMapper

--- src/main/java/ru/skypro/homework/mapper/UserMapper.java
+++ src/main/java/ru/skypro/homework/mapper/UserMapper.java
@@ -1,14 +1,39 @@
 package ru.skypro.homework.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import ru.skypro.homework.dto.User;
 import ru.skypro.homework.model.UserModel;
 
+import java.util.List;
+
 @Mapper (componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
 
-    UserModel toUserModel(User user);
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "email", source = "email")
+    @Mapping(target = "firstName", source = "firstName")
+    @Mapping(target = "lastName", source = "lastName")
+    @Mapping(target = "phone", source = "phone")
+    @Mapping(target = "role", source = "role")
+    @Mapping(target = "image", source = "image")
+    User toDto(UserModel userModel);
 
-    User toUserDto(UserModel userModel);
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "email", source = "email")
+    @Mapping(target = "firstName", source = "firstName")
+    @Mapping(target = "lastName", source = "lastName")
+    @Mapping(target = "phone", source = "phone")
+    @Mapping(target = "role", source = "role")
+    @Mapping(target = "image", source = "image")
+    @Mapping(target = "username", source = "email")
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "ads", ignore = true)
+    @Mapping(target = "comments", ignore = true)
+    UserModel toModel(User user);
+
+    List<User> toDtoList(List<UserModel> userModels);
+
+    List<UserModel> toModelList(List<User> users);
 }

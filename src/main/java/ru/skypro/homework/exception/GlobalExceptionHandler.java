5bd0456e4d860df9ee736c6595e42a97b13b667f
Aleksei <alexejsedow@gmail.com>
2025-05-06 23:52:56 +0300

    GlobalExceptionHandler  добавил для удобного обнаружения ошибок

---
+++ src/main/java/ru/skypro/homework/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package ru.skypro.homework.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAllExceptions(Exception ex) {
+        // Логируем ошибку
+        System.err.println("Error occurred: " + ex.getMessage());
+
+        // Возвращаем ответ с ошибкой
+        return new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
